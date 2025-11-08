6929d3e675367d613e840af1bb6bcc6173791431
Farhan Rana <rana.tdm@gmail.com>
2025-11-08 22:58:24 +0500

    s

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-from langchain_core.messages import HumanMessage
+from langchain.schema import HumanMessage
 from langchain_openai import ChatOpenAI
 from langchain.tools import tool
 from langgraph.prebuilt import create_react_agent
